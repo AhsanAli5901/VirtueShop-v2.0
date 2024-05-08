196a2741266dec53ff5f5c338463e650b0f620a4
Ahsan Ali <ahsanali5901@gmail.com>
2024-05-08 12:13:18 +0500

    Deleting testing files

--- frontend/__test__/Login.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Login from '../src/component/User/Login';
-
-test('renders Login component without crashing', () => {
-  render(<Login />);
-});
